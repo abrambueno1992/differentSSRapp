070f9825abb5db516e01884b66be8c9d9cb9af66
Abraham Bueno <abueno@localhost.localdomain>
2018-10-18 23:38:34 -0500

    Made changes to refactor index.js to es5

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -8906,14 +8906,26 @@ module.exports = memoizeStringOnly;
 "use strict";
 
 
-var express = __webpack_require__(68);
-var React = __webpack_require__(28);
-var renderToString = __webpack_require__(143).renderToString;
-var Home = __webpack_require__(152).default;
-var app = express();
+var _express = __webpack_require__(68);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(28);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(143);
+
+var _Home = __webpack_require__(152);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-    var content = renderToString(React.createElement(Home, null));
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
     res.send(content);
 });
