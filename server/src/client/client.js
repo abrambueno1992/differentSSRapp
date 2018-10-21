dc2c5ee26b6cee085796ac7ece5d0b0a8abcee1a
Abraham Bueno <abueno@localhost.localdomain>
2018-10-20 22:43:56 -0500

    Added UserList component.

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -8,7 +8,11 @@ import { BrowserRouter } from 'react-router-dom';
 import Routes from './Routes';
 import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(thunk));
+const store = createStore(reducers, 
+    {},
+    window.__REDUX_DEVTOOLS_EXTENSION__ && 
+    window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk));
 
 
 ReactDOM.hydrate(
